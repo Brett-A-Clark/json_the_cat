c71acb4f816d0f9408b30ac13e78a9f8cbf18270
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-08-01 20:49:01 +0000

    Final update

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,19 @@
 const request = require('request');
 
-const breed = process.argv[2];
-
-// ?q=value => query parameter
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (response === undefined) {
-    console.log(error); 
-  } else {
-    if (response.statusCode === 200 && !error) {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      return callback(error);
+    } else {
       const data = JSON.parse(body);
-      if (!data[0]) {
-        console.log("The requested breed was not found.")
+      if (data.length < 1) {
+        return callback('The requested breed was not found.');
+      } else {
+        return callback(data[0].description);
+      }
     }
-    console.log(data[0].description);
-  }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };;
+  
